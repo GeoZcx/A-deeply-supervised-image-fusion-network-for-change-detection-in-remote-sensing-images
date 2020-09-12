918b331a373923fa69be9a552c4be76dae982ec2
Chenxiao Zhang <42008403+GeoZcx@users.noreply.github.com>
2020-09-12 20:38:59 +0800

    Create loss.py

---
+++ pytorch version/loss.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+
+def cd_loss(input,target):
+    bce_loss = nn.BCELoss()
+    bce_loss = bce_loss(torch.sigmoid(input),torch.sigmoid(target))
+
+    smooth = 1.
+    iflat = input.view(-1)
+    tflat = target.view(-1)
+    intersection = (iflat * tflat).sum()
+    dic_loss = 1 - ((2. * intersection + smooth)/(iflat.sum() + tflat.sum() + smooth))
+
+    return  dic_loss + bce_loss
